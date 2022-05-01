260298b5db06262d1cd453b145f84fdc1ceecb87
MinaYuni <minhanh.ledoan@gmail.com>
2022-05-01 17:33:31 +0200

    add StateStopFSMBehaviour

---
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/fsm/StateStopFSMBehaviour.java
@@ -0,0 +1,30 @@
+package eu.su.mas.dedaleEtu.mas.behaviours.fsm;
+
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import jade.core.AID;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class StateStopFSMBehaviour extends OneShotBehaviour {
+    private static final long serialVersionUID = 8567689731499797561L;
+
+    private MapRepresentation myMap;
+    private List<String> list_agentNames;
+    private HashMap<String, HashMap<String, Boolean>> dictVoisinsMessages;
+    private int exitValue;
+
+    public StateStopFSMBehaviour(final AbstractDedaleAgent myagent, MapRepresentation myMap, List<String> agentNames, HashMap<String, HashMap<String, Boolean>> dico) {
+        super(myagent);
+        this.myMap=myMap;
+        this.list_agentNames=agentNames;
+        this.dictVoisinsMessages = dico;
+    }
+
+    public void action() {
+        System.out.println("StateStopFSMBehaviour (state F) : " + this.myAgent.getLocalName());
+    }
+}
